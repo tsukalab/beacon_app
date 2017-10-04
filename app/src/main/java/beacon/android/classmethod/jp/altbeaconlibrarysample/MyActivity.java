ea19e90650b76b8b7f29f077cc682f5b0bfaee43
FuutoSeino <f1014072@gmail.com>
2017-10-04 17:35:58 +0900

    first commit

---
+++ app/src/main/java/beacon/android/classmethod/jp/altbeaconlibrarysample/MyActivity.java
@@ -0,0 +1,122 @@
+package beacon.android.classmethod.jp.altbeaconlibrarysample;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.Region;
+
+
+public class MyActivity extends Activity implements BeaconConsumer {
+
+    private BeaconManager beaconManager;
+
+    // iBeaconのデータを認識するためのParserフォーマット
+    public static final String IBEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+        // staticメソッドで取得
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+
+        // BeaconParseを設定
+        beaconManager.getBeaconParsers()
+                .add(new BeaconParser().setBeaconLayout(IBEACON_FORMAT));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        beaconManager.unbind(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        beaconManager.bind(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.setMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(Region region) {
+                // 領域への入場を検知
+                Log.d("Beacon", "ENTER Region.");
+            }
+
+            @Override
+            public void didExitRegion(Region region) {
+                // 領域からの退場を検知
+                Log.d("Beacon", "EXIT Region. ");
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int i, Region region) {
+                // 領域への入退場のステータス変化を検知
+                Log.d("MyActivity", "DetermineState: " + i);
+            }
+        });
+
+        try {
+            // ビーコン情報の監視を開始
+            beaconManager.startMonitoringBeaconsInRegion(new Region("unique-id-001", null, null, null));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
+            return rootView;
+        }
+    }
+}
